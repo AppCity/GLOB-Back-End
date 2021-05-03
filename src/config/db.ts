58e9e72f00c8beb61e3910afaeab8efb7e97168e
stickfox <stickfoxxx@gmail.com>
2021-05-03 23:27:07 +0200

    Fixed db string.

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,9 +8,11 @@ const {
     MONGO_DATABASE = 'auth'
 } = process.env
 
-export const MONGO_URI = `mongodb://${MONGO_USERNAME}:${
-    encodeURIComponent(MONGO_PASSOWORD)
-}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`
+export const MONGO_URI = (process.env.PORT)
+    ? "mongodb+srv://test:test@offerwriterbotcluster.2ow6r.mongodb.net/offerswriterdb?retryWrites=true&w=majority"
+    : `mongodb://${MONGO_USERNAME}:${
+        encodeURIComponent(MONGO_PASSOWORD)
+        }@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`
 
 export const MONGO_OPTIONS: ConnectionOptions = {
     useNewUrlParser: true,
--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,6 +1,3 @@
 export * from './auth'
 
 export * from './joi'
-
-
-// CAMBIARE @hapi/joi con la versione non deprecata!!! poi prova push su heroku con nuovi moduli
