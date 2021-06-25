414b2f1ce6c8b007f8d61885408051610d188faa
stickfox <stickfoxxx@gmail.com>
2021-06-26 01:03:59 +0200

    CU-gz8m35 - Added error message on req validation.

--- src/validation/joi.js
+++ src/validation/joi.js
@@ -1,10 +1,19 @@
-const { BadRequest } = require("../errors")
+const { BadRequest } = require('../errors')
 
 const validate = async (schema, payload) => {
     try {
+        // validate the received message using the validation/auth joi model
         await schema.validateAsync(payload, { abortEarly: false})
     } catch (e) {
-        throw new BadRequest(e)
+        // write the error string with missing things on the message
+        const details = e.details
+        let errorMessage = []
+
+        details.forEach(element => {
+            errorMessage.push(element.message);
+        });
+
+        throw new BadRequest(errorMessage)
     }
 }
 
