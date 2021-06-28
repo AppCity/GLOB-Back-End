4e9f5812da68797d2a18719b37114bbf19c753d1
Giovanni Volpin <giovannivolpin@gmail.com>
2021-06-28 18:08:12 +0200

    Added NotLoggedIn message when no refreshToken.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,6 @@
 const { isLoggedIn, logOut } = require('../auth')
 const { Unauthorized } = require('../errors')
+const { User } = require('../models/user')
 
 // method which says if a user is already logged in
 const guest = (req, res, next) => {
@@ -11,13 +12,21 @@ const guest = (req, res, next) => {
 }
 
 // method which says if a user don't have access to content because is not logged in
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
+    const notLoggedIn = 'NotLoggedIn'
+
     const { loginResult, errorName } = isLoggedIn(req, req.url.includes("token"))
 
     if (!loginResult) {
-        return next(new Unauthorized((errorName) ? errorName : 'NotLoggedIn'))
+        return next(new Unauthorized((errorName) ? errorName : notLoggedIn))
     }
+
+    // validate if the user has been logged out
+    const user = await User.findById(req.user.id)
     
+    if (!user.refreshToken)
+        return next(new Unauthorized(notLoggedIn))
+
     next()
 }
 
