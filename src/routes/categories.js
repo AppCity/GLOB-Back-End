a643d862c489602df277b9e02172e544ae6a330a
Giovanni Volpin <giovannivolpin@gmail.com>
2021-09-15 00:50:12 +0200

    CU-12gteqd Add POST request to create new category

---
+++ src/routes/categories.js
@@ -0,0 +1,42 @@
+const { Router } = require("express")
+const { guest, auth } = require("../middleware/auth")
+const { catchAsync } = require("../middleware/errors")
+const { Category } = require("../models/category")
+const { User } = require("../models/user")
+const { Unauthorized, BadRequest } = require("../errors/index")
+const { RESPONSE_STATUS_OK, RESPONSE_STATUS_ERROR } = require("../config/constants")
+
+const router = Router()
+
+/**
+ * Create a new category
+ */
+router.post('/categories', auth, catchAsync(async (req, res) => {
+    const { value, title, image } = req.body
+
+    // search if a category with the same value exists
+    const valueFound = await Category.exists({ value })
+
+    if (valueFound) {
+        throw new BadRequest('Category with same value exists yet!')
+    }
+
+    // TODO how to add the image.
+
+    // create new category and save it inside the DB
+    const category = await Category.create({
+        value,
+        title,
+        image
+    })
+
+    console.log("NEW Category: ", category)
+
+    // respond with the two tokens
+    res.json({ message: RESPONSE_STATUS_OK })
+
+    res.end()
+
+    return true
+}))
+
