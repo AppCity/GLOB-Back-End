e5c097e26e797a2761d63d5bf1e7743dc16efae1
Giovanni Volpin <giovannivolpin@gmail.com>
2021-09-25 22:23:12 +0200

    CU-11m5h80 Added like and favorite lists to user

--- src/utils/userUtils.js
+++ src/utils/userUtils.js
@@ -1,12 +1,17 @@
 const { Blog } = require("../models/blog")
+const { Like } = require("../models/like")
+const { Favorite } = require("../models/favorite");
 
-const { RESPONSE_STATUS_OK } = require("../config/constants")
+const { RESPONSE_STATUS_OK } = require("../config/constants");
 
 /**
  * Return all the details of the given user to respond to auth requests
  */
 const getUserDetails = async (user, accessToken) => {
     const publishedArticles = await getPublishedArticlesNumber(user._id);
+
+    const likedBlogs = await getUserLikes(user._id)
+    const favoritedBlogs = await getUserFavorites(user._id)
 
     return {
         message: RESPONSE_STATUS_OK,
@@ -19,7 +24,9 @@ const getUserDetails = async (user, accessToken) => {
         profileImage: user.image,
         website: (user.website) ? user.website : "",
         dailyReaders: (user.dailyReaders) ? user.dailyReaders : 0,
-        publishedArticles
+        publishedArticles,
+        likes: likedBlogs,
+        favorites: favoritedBlogs
     }
 }
 
@@ -31,10 +38,16 @@ const getPublishedArticlesNumber = async (userId) => {
 }
 
 /**
+ * Return all liked blogs of given user.
+ */
+const getUserLikes = async (userId) => {
+    return (await Like.find({ userId, active: true }).sort({ createdAt:-1 }).select('blogId')).map((liked) => liked.blogId)
+}
+/**
  * Return all favorites blogs of given user.
  */
 const getUserFavorites = async (userId) => {
-    
+    return (await Favorite.find({ userId, favorite: true }).sort({ createdAt:-1 }).select('blogId')).map((favorite) => favorite.blogId)
 }
 
 module.exports = {
